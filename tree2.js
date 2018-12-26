6311d7b4a09d871bd3c28271564f578a8b013f14
Stephen Smith <stephen@mapsmith.net>
2018-12-26 16:44:44 -0600

    change from angle constraint

--- tree2.js
+++ tree2.js
@@ -16,8 +16,9 @@ const sketch = () => {
 
 
     // grid setup
-    const rows = 6;
-    const cols = 6;
+    const rows = random.rangeFloor(2,5);
+    const cols = random.rangeFloor(2,5);
+    console.log(`rows: ${rows}, cols: ${cols}`);
     const cellWidth = (printWidth / cols) - cellBuffer;
     const cellHeight = (printHeight / rows) - cellBuffer;
     const cellInnerMargin = 25
@@ -42,16 +43,53 @@ const sketch = () => {
         // context.strokeRect(i, j, cellWidth, cellHeight)
 
         // random tree parameters
-        const steps = random.rangeFloor(5,8); // number of triangles on the tree
-        const angle = random.range(50, 60); // the top angle of the triangle
+        const steps = random.rangeFloor(10,20); // number of triangles on the tree
+        const angle = random.range(60, 60); // the top angle of the triangle
 
         // set a unique color for each tree
         var sat = random.range(40, 80) // saturation. higher is pastel-er
         var light = random.range(40, 60) // lightness. higher is pastel-er
         var green = random.range(105, 130); // green hues
         var brown = random.range(25, 40); // brown hues
-        var treeGreen = `hsl(${green}, ${sat}%, ${light}%)`;
+        var treeGreen = `hsla(${green}, ${sat}%, ${light}%, 1)`;
         var trunkBrown = `hsl(${brown}, 100%, 40%)`;
+
+
+        var treeHeight = cellHeight * 1.75
+        var overlap = 0.5;
+        var treeWidth = cellWidth - (cellInnerMargin * 2)
+        startX = i + (cellWidth / 2); // this is only set once per tree
+
+        for (var k = 1; k <= steps; k++) {
+          var segmentWidth = k / steps * treeWidth;
+          var segmentHeight = treeHeight / steps;
+
+          if (k === 1) {
+            startY = j;
+          } else {
+            startY += segmentHeight * overlap;
+          }
+
+          y1 = startY + segmentHeight
+          x1 = startX - (segmentWidth / 2)
+
+          y2 = startY + segmentHeight
+          x2 = startX + (segmentWidth / 2)
+
+          triangle(startX, startY, x1, y1, x2, y2, treeGreen);
+
+          if (k === steps) {
+            var bottom = j + cellHeight;
+            var trunkHeight = bottom - y2;
+            var trunkWidth = (cellWidth - (cellInnerMargin * 2)) / 6
+            var trunkStart = startX - (trunkWidth / 2);
+            trunk(trunkStart, y1, trunkWidth, trunkHeight, trunkBrown)
+          }
+
+
+          // overlap
+
+        }
 
         /*
           this loop draws the tree
@@ -65,16 +103,17 @@ const sketch = () => {
 
           the distance between the stars is the radius
         */
+        /*
         for (var k = 1; k <= steps; k++) {
           if (k === 1) { // on the first triangle, variables are set differently
             shift = (cellWidth / steps);
             startX = i + (cellWidth / 2); // this is only set once per tree
-            startY = j + cellInnerMargin;
+            startY = j;
           } else {
-            shift = ((cellWidth - (cellInnerMargin * 2)) / steps);
+            shift = ((cellHeight - cellInnerMargin) / steps / 1.675);
             startY += shift;
           }
-          r = shift * k
+          r = k * shift
           // point A
           x1 = startX - (r * Math.cos(math.degToRad(angle)))
           y1 = startY + (r * Math.sin(math.degToRad(angle)))
@@ -94,7 +133,7 @@ const sketch = () => {
             trunk(trunkStart, y1, trunkWidth, trunkHeight, trunkBrown)
           }
         }
-
+          */
       }
     }
 
